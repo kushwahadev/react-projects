8a87b782c235b265bb7b2b185884c23ba1bedf73
Dev Kushwaha <devkushwaha3575@gmail.com>
2024-03-23 18:32:45 +0530

    basic and latest react installation

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h2>react</h2>;
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>vite react</h1>
+    </>
+  );
+}
+
+export default App;
