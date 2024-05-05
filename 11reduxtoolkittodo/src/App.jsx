8a5f79ba39e1479dc8e726eef5875d502ab89d50
Dev Kushwaha <devkushwaha3575@gmail.com>
2024-05-05 11:14:54 +0530

    redux toolkit todo

---
+++ 11reduxtoolkittodo/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todo";
+
+function App() {
+  return (
+    <>
+      <h1>Learn about redux</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
