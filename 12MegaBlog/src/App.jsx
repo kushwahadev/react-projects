9a6f1da68edde7487dcd6e31306a1dd806190b3a
Dev Kushwaha <devkushwaha3575@gmail.com>
2024-05-27 23:41:41 +0530

    mega project of blog app

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className=" min-h-screen flex flex-wrap content-between bg-gray-400">
+      <Header />
+      <main>
+        TODO : <Outlet />
+      </main>
+      <Footer />
+    </div>
+  ) : null;
+}
+
+export default App;
---
+++ 12MegaBlog/src/components/Logo.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+function Logo({ widhth = "100px" }) {
+  return <div>Logo</div>;
+}
+
+export default Logo;
